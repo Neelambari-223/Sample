18430b0ed10a4c9a8552c2b9d61543b95697f795
Neelambari-223 <79809779+Neelambari-223@users.noreply.github.com>
2021-03-10 09:55:52 +0530

    Add files via upload

---
+++ LoginApp/loginapp/src/main/java/com/cg/loginapp/idclass/UserId.java
@@ -0,0 +1,38 @@
+package com.cg.loginapp.idclass;
+/**
+ * @author Neelambari k
+ */
+import java.io.Serializable;
+
+public class UserId implements Serializable {
+    private String emailId;
+	private String userType;
+
+    public UserId()
+    {
+    	
+    }
+
+    public UserId(String emailid, String usertype) {
+        this.emailId = emailid;
+        this.userType = usertype;
+    }
+   
+    public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+}
+
